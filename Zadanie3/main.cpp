07498863824955961ae9fe8f14e78fa6b43f1dc2
Zhenya Nikiforov <12A92@mail.ru>
2025-10-05 11:43:58 +0500

    Третье задание первоначально

--- Zadanie3/main.cpp
+++ Zadanie3/main.cpp
@@ -2,20 +2,36 @@
 #include <Windows.h>
 using namespace std;
 
-class Figure
+class Figure //-Базовый класс;
 {
 public:
   Figure(int sides)
   {
     this->sides = sides;
   }
-  int getSides() { return this->sides; }
+
+  virtual void print_info() //-Выводим информацию о фигуре;
+  {
+    SetConsoleOutputCP(CP_UTF8);
+    cout << "Количество сторон: " << this->sides << "\n";
+  }
+
+  virtual bool check() //-Проверяем правильность фигуры;
+  {
+    if (this->sides == 0)
+    {
+      return true;
+    };
+    return false;
+  }
+
+  virtual ~Figure() {}; //-Виртуальный деструктор
 
 protected:
   int sides;
 };
 
-class Triangle : public Figure
+class Triangle : public Figure //-Класс обычного треугольника
 {
 public:
   Triangle(int a, int b, int c, int A, int B, int C, int sides) : Figure(sides)
@@ -27,12 +43,23 @@ public:
     this->B = B;
     this->C = C;
   }
-  int geta() { return this->a; }
-  int getb() { return this->b; }
-  int getc() { return this->c; }
-  int getA() { return this->A; }
-  int getB() { return this->B; }
-  int getC() { return this->C; }
+
+  void print_info() override //-Выводим информацию о треугольнике;
+  {
+    SetConsoleOutputCP(CP_UTF8);
+    cout << "Количество сторон: " << this->sides << "\n";
+    cout << "Стороны: " << "a=" << this->a << " b=" << this->b << " c=" << this->c << "\n";
+    cout << "Углы: " << "A=" << this->A << " B=" << this->B << " C=" << this->C << "\n";
+  }
+
+  bool check() override //-Проверяем правильность треугольника;
+  {
+    if ((this->A + this->B + this->C == 180) && (this->sides == 3))
+    {
+      return true;
+    };
+    return false;
+  }
 
 protected:
   int a;
@@ -43,7 +70,7 @@ protected:
   int C;
 };
 
-class Quadrangle : public Triangle
+class Quadrangle : public Triangle //-Класс четырёхугольника;
 {
 public:
   Quadrangle(int a, int b, int c, int d, int A, int B, int C, int D, int sides) : Triangle(a, b, c, A, B, C, sides)
@@ -51,26 +78,64 @@ public:
     this->d = d;
     this->D = D;
   }
-  int getd() { return this->d; }
-  int getD() { return this->D; }
+
+  void print_info() override //-Выводим информацию о четырёхугольнике;
+  {
+    SetConsoleOutputCP(CP_UTF8);
+    cout << "Количество сторон: " << this->sides << "\n";
+    cout << "Стороны: " << "a=" << this->a << " b=" << this->b << " c=" << this->c << " d=" << this->d << "\n";
+    cout << "Углы: " << "A=" << this->A << " B=" << this->B << " C=" << this->C << " D=" << this->D << "\n";
+  }
+
+  bool check() override //-Проверяем правильность четырёхугольника;
+  {
+    if ((this->A + this->B + this->C + this->D == 360) && (this->sides == 4))
+    {
+      return true;
+    };
+    return false;
+  }
 
 protected:
   int d;
   int D;
 };
 
+void showInfo(Figure *f) //-Выводим стороны и углы;
+{
+  f->print_info();
+};
+
+void showTrueOrFalse(Figure *f) //-Выводим правильность;
+{
+  SetConsoleOutputCP(CP_UTF8);
+  if (f->check())
+  {
+    cout << "Правильная\n";
+    return;
+  }
+  cout << "Неправильная\n";
+};
+
 int main()
 {
   SetConsoleOutputCP(CP_UTF8);
 
-  Figure fg(0);
-  Triangle tr(10, 20, 30, 50, 60, 70, 3);
-  Quadrangle qa(10, 20, 30, 40, 50, 60, 70, 80, 4);
+  Figure fg(0);                                     //-Экземпляр фигуры;
+  Triangle tr(10, 20, 30, 50, 60, 70, 3);           //-Экземпляр треугольника;
+  Quadrangle qa(10, 20, 30, 40, 50, 60, 70, 80, 4); //-Экземпляр четырёхугольника;
 
   cout << "Фигура:\n";
-  cout << "Количество сторон: " << fg.getSides() << endl;
-  cout << "\nТреугольник\n";
-  cout << "Количество сторон: " << tr.getSides() << endl;
-  cout << "Стороны: a=" << tr.geta() << " b=" << tr.getb() << " c=" << tr.getc() << endl;
+  showTrueOrFalse(&fg);
+  showInfo(&fg);
+
+  cout << "\nТреугольник:\n";
+  showTrueOrFalse(&tr);
+  showInfo(&tr);
+
+  cout << "\nЧетырёхугольник:\n";
+  showTrueOrFalse(&qa);
+  showInfo(&qa);
+
   return 0;
 }
