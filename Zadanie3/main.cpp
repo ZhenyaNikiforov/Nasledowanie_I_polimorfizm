7b7e82a3ca7bce3a2b7bed6d83fba52f99e6f41e
Zhenya Nikiforov <12A92@mail.ru>
2025-10-05 14:35:09 +0500

    Третье задание окончательно

--- Zadanie3/main.cpp
+++ Zadanie3/main.cpp
@@ -70,6 +70,52 @@ protected:
   int C;
 };
 
+class RightTriangle : public Triangle //-Класс прямоугольного треугольника;
+{
+public:
+  using Triangle::Triangle;
+
+  bool check() override //-Проверяем правильность прямоугольного треугольника;
+  {
+    if ((this->A + this->B + this->C == 180) && (this->sides == 3) && (this->C == 90))
+    {
+      return true;
+    };
+    return false;
+  }
+};
+
+class IsoscelesTriangle : public Triangle //-равнобедренный треугольник
+{
+public:
+  using Triangle::Triangle;
+  bool check() override //-Проверяем правильность равнобедренного треугольника;
+  {
+    if ((this->A + this->B + this->C == 180) && (this->sides == 3) && (this->A == this->C) && (this->a == this->c))
+    {
+      return true;
+    };
+    return false;
+  }
+};
+
+class EquilateralTriangle : public Triangle //-Равносторонний треугольник
+{
+public:
+  using Triangle::Triangle;
+  bool check() override //-Проверяем правильность равностороннего треугольника;
+  {
+    bool parityABC = (this->A == this->B) && (this->B == this->C) && (this->A == 60);
+    bool parityabc = (this->a == this->b) && (this->b == this->c);
+
+    if ((this->A + this->B + this->C == 180) && (this->sides == 3) && parityABC && parityabc)
+    {
+      return true;
+    };
+    return false;
+  }
+};
+
 class Quadrangle : public Triangle //-Класс четырёхугольника;
 {
 public:
@@ -101,6 +147,70 @@ protected:
   int D;
 };
 
+class Rectangl : public Quadrangle //-Класс прямоугольник;
+{
+public:
+  using Quadrangle::Quadrangle;
+  bool check() override //-Проверяем правильность прямоугольника;
+  {
+    bool parityABCD = (this->A == this->B) && (this->B == this->C) && (this->C == this->D);
+    bool parityabcd = (this->a == this->c) && (this->b == this->d);
+    if ((this->A + this->B + this->C + this->D == 360) && (this->sides == 4) && parityABCD && parityabcd)
+    {
+      return true;
+    };
+    return false;
+  }
+};
+
+class Square : public Quadrangle //-Класс квадрат;
+{
+public:
+  using Quadrangle::Quadrangle;
+  bool check() override //-Проверяем правильность квадрата;
+  {
+    bool parityABCD = (this->A == this->B) && (this->B == this->C) && (this->C == this->D) && (this->D == 90);
+    bool parityabcd = (this->a == this->b) && (this->b == this->c) && (this->c == this->d);
+    if ((this->A + this->B + this->C + this->D == 360) && (this->sides == 4) && parityABCD && parityabcd)
+    {
+      return true;
+    };
+    return false;
+  }
+};
+
+class Paralelogram : public Quadrangle //-Класс параллелограмм;
+{
+public:
+  using Quadrangle::Quadrangle;
+  bool check() override //-Проверяем правильность параллелограмма;
+  {
+    bool parityABCD = (this->A == this->C) && (this->B == this->D);
+    bool parityabcd = (this->a == this->c) && (this->b == this->d);
+    if ((this->A + this->B + this->C + this->D == 360) && (this->sides == 4) && parityABCD && parityabcd)
+    {
+      return true;
+    };
+    return false;
+  }
+};
+
+class Rhomb : public Quadrangle //-Класс ромб;
+{
+public:
+  using Quadrangle::Quadrangle;
+  bool check() override //-Проверяем правильность ромба;
+  {
+    bool parityABCD = (this->A == this->C) && (this->B == this->D);
+    bool parityabcd = (this->a == this->b) && (this->b == this->c) && (this->c == this->d);
+    if ((this->A + this->B + this->C + this->D == 360) && (this->sides == 4) && parityABCD && parityabcd)
+    {
+      return true;
+    };
+    return false;
+  }
+};
+
 void showInfo(Figure *f) //-Выводим стороны и углы;
 {
   f->print_info();
@@ -121,9 +231,17 @@ int main()
 {
   SetConsoleOutputCP(CP_UTF8);
 
-  Figure fg(0);                                     //-Экземпляр фигуры;
-  Triangle tr(10, 20, 30, 50, 60, 70, 3);           //-Экземпляр треугольника;
-  Quadrangle qa(10, 20, 30, 40, 50, 60, 70, 80, 4); //-Экземпляр четырёхугольника;
+  Figure fg(0);                                       //-Экземпляр фигуры;
+  Triangle tr(10, 20, 30, 50, 60, 70, 3);             //-Экземпляр треугольника;
+  RightTriangle rtr1(10, 20, 30, 50, 60, 90, 3);      //-Прямоугольный треугольник-1;
+  RightTriangle rtr2(10, 20, 30, 50, 40, 90, 3);      //-Прямоугольный треугольник-2;
+  IsoscelesTriangle itr(10, 20, 10, 50, 60, 50, 3);   //-Равнобедренный треугольник
+  EquilateralTriangle etr(30, 30, 30, 60, 60, 60, 3); //-Равносторонний треугольник
+  Quadrangle qa(10, 20, 30, 40, 50, 60, 70, 80, 4);   //-Экземпляр четырёхугольника;
+  Rectangl rc(10, 20, 10, 20, 90, 90, 90, 90, 4);     //-Прямоугольник
+  Square sq(20, 20, 20, 20, 90, 90, 90, 90, 4);       //-Квадрат;
+  Paralelogram pg(20, 30, 20, 30, 30, 40, 30, 40, 4); //-Параллелограмм;
+  Rhomb rb(30, 30, 30, 30, 30, 40, 30, 40, 4);        //-Ромб;
 
   cout << "Фигура:\n";
   showTrueOrFalse(&fg);
@@ -133,9 +251,41 @@ int main()
   showTrueOrFalse(&tr);
   showInfo(&tr);
 
+  cout << "\nПрямоугольный треугольник:\n";
+  showTrueOrFalse(&rtr1);
+  showInfo(&rtr1);
+
+  cout << "\nПрямоугольный треугольник:\n";
+  showTrueOrFalse(&rtr2);
+  showInfo(&rtr2);
+
+  cout << "\nРавнобедренный треугольник\n";
+  showTrueOrFalse(&itr);
+  showInfo(&itr);
+
+  cout << "\nРавносторонний треугольник\n";
+  showTrueOrFalse(&etr);
+  showInfo(&etr);
+
   cout << "\nЧетырёхугольник:\n";
   showTrueOrFalse(&qa);
   showInfo(&qa);
+
+  cout << "\nПрямоугольник\n";
+  showTrueOrFalse(&rc);
+  showInfo(&rc);
+
+  cout << "\nКвадрат\n";
+  showTrueOrFalse(&sq);
+  showInfo(&sq);
+
+  cout << "\nПараллелограмм\n";
+  showTrueOrFalse(&pg);
+  showInfo(&pg);
+
+  cout << "\nРомб\n";
+  showTrueOrFalse(&rb);
+  showInfo(&rb);
 
   return 0;
 }
